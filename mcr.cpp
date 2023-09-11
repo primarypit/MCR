0ab6c3f73bf3fdc433821ef94d1c33988e9ddd54
CHEN Xuwei <144666126+xuweichen2@users.noreply.github.com>
2023-09-11 20:43:08 +0800

    Update mcr.cpp

--- mcr.cpp
+++ mcr.cpp
@@ -1,5 +1,6 @@
 bool isWin(char game[3][3]){
 	bool win = false;
+	//Change 01
 	if (game[0][0] == game[0][1] && game[0][1] == game[0][2] && (game[0][0] == 'X' || game[0][0] == 'O')) win = true;
 	if (game[1][0] == game[1][1] && game[1][1] == game[1][2] && (game[1][0] == 'X' || game[1][0] == 'O')) win = true;
 	if (game[2][0] == game[2][1] && game[2][1] == game[2][2] && (game[2][0] == 'X' || game[2][0] == 'O')) win = true;
